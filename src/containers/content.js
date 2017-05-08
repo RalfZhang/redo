17560302139a2322c10e6753b477c5a60614c87c
unknown <zhangkang103@gmail.com>
2017-05-09 03:24:18 +0800

    更新

---
+++ src/containers/content.js
@@ -0,0 +1,17 @@
+/**
+ * Body
+ * @authors RalfZ (ralfz.zhang@gmail.com)
+ * @date    2017-05-09 01:15:14
+ * @version 1.0.0
+ */
+
+import React, { Component } from 'react';
+
+
+export default class Content extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
