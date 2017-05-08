95c57dff28c3e6c65d3645e885a1b1e932623f4d
unknown <zhangkang103@gmail.com>
2017-05-09 03:23:28 +0800

    更新头部

--- src/containers/header.js
+++ src/containers/header.js
@@ -6,6 +6,25 @@
  */
 
 import React, { Component } from 'react';
+import styles from './style.css';
+
+const colorStyle = {
+  light: {
+    color: 'rgb(255, 198, 47)'
+  },
+  green: {
+    color: 'rgb(138, 158, 120)'
+  },
+  blue: {
+    color: 'rgb(121, 130, 131)'
+  },
+  red: {
+    color: 'rgb(204, 127, 102)'
+  },
+  yellow: {
+    color: 'rgb(219, 152, 51)'
+  }
+}
 
 export default class Header extends Component {
   constructor(...args) {
@@ -18,16 +37,16 @@ export default class Header extends Component {
   tabClick(e) {
     console.log('tab is clicked', e)
   }
-  
+
   render() {
     return (
-      <div className="hearder">
-        <div className="logo">Redo</div>
-        <div>
-          <div className="tab" onClick={this.tabClick.bind(this)}>电影</div>
-          <div className="tab" onClick={this.tabClick.bind(this)}>读书</div>
-          <div className="tab" onClick={this.tabClick.bind(this)}>音乐</div>
-          <div className="tab" onClick={this.tabClick.bind(this)}>一刻</div>
+      <div className={styles.header} >
+        <div className={styles.logo}  style={colorStyle.light}>Redo</div>
+        <div className={styles.tab}>
+          <div className={styles.item} style={colorStyle.blue} onClick={this.tabClick.bind(this)}>电影</div>
+          <div className={styles.item} style={colorStyle.red} onClick={this.tabClick.bind(this)}>读书</div>
+          <div className={styles.item} style={colorStyle.yellow} onClick={this.tabClick.bind(this)}>音乐</div>
+          <div className={styles.item} style={colorStyle.green} onClick={this.tabClick.bind(this)}>一刻</div>
         </div>
       </div>
     );
