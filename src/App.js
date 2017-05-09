df3eebcac1da06b7f39b4d65d28b01d655201d7a
unknown <zhangkang103@gmail.com>
2017-05-09 23:39:53 +0800

    show content

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default class App extends Component {
     return (
       <div>
         <Header />
-        {/*<Content />*/}
+        <Content />
         {/*<Footer />*/}
 
       </div>
