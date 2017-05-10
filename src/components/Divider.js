f48053e462ccc2c6a27707a9855f5d91b44ba3f9
unknown <zhangkang103@gmail.com>
2017-05-10 23:33:25 +0800

    add divider component

---
+++ src/components/Divider.js
@@ -0,0 +1,16 @@
+/**
+ * 分割线
+ * @authors RalfZ (ralfz.zhang@gmail.com)
+ * @date    2017-05-10 08:47:53
+ * @version 1.0.0
+ */
+
+import React, { Component } from 'react';
+
+export default class Divider extends Component {
+  render(){
+    return (
+      <div>divider</div>
+    )
+  }
+}
