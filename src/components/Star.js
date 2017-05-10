3cb9f3c0fee1019059788c28d7d12d4cc1dced1f
unknown <zhangkang103@gmail.com>
2017-05-10 23:33:34 +0800

    add star component

---
+++ src/components/Star.js
@@ -0,0 +1,16 @@
+/**
+ * 评分星星
+ * @authors RalfZ (ralfz.zhang@gmail.com)
+ * @date    2017-05-10 22:30:04
+ * @version 1.0.0
+ */
+
+import React, { Component } from 'react'
+
+export default class Star extends Component {
+  render(){
+    return (
+      <div>xing</div>
+    )
+  }
+}
