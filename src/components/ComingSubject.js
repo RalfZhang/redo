b9e5d3470a05a46f9ae9ea1d45337aa18f134daf
unknown <zhangkang103@gmail.com>
2017-05-10 23:33:10 +0800

    add subject tmpl

---
+++ src/components/ComingSubject.js
@@ -0,0 +1,22 @@
+/**
+ * 即将上映
+ * @authors RalfZ (ralfz.zhang@gmail.com)
+ * @date    2017-05-10 08:30:54
+ * @version 1.0.0
+ */
+
+import React, { Component } from 'react';
+
+export default class ComingSubject extends Component {
+  render() {
+    return (
+      
+      
+            <div className="subject">
+              <div className="img"></div>
+              <div className="name"></div>
+              <div className="time"></div>
+            </div>
+    )
+  }
+}
---
+++ src/components/Subject.js
@@ -0,0 +1,22 @@
+/**
+ * 单个小图
+ * @authors RalfZ (ralfz.zhang@gmail.com)
+ * @date    2017-05-10 08:25:59
+ * @version 1.0.0
+ */
+
+
+import React, { Component } from 'react';
+
+export default class Subject extends Component {
+  render(){
+    return (
+      
+            <div className="subject">
+              <div className="img"></div>
+              <div className="name"></div>
+              <div className="star"></div>
+            </div>
+    )
+  }
+}
