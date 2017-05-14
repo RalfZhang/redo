73f534561ce91e698a8413851abaa7a082718d4d
unknown <zhangkang103@gmail.com>
2017-05-14 23:47:33 +0800

    路由更新

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+
+import {
+  Route,
+} from 'react-router-dom'
+
 import Header from './containers/header';
 import Footer from './containers/footer.js';
 import Content from './containers/content.js';
 
+import Movie from './containers/movie/Movie.js'
+
 import './App.css';
+
 
 
 export default class App extends Component {
@@ -11,7 +19,11 @@ export default class App extends Component {
     return (
       <div>
         <Header />
-        <Content />
+        {/*<Content></Content>*/}
+        <div className="content">
+          
+          <Route pattern="/movie" component={Movie} />
+        </div>
         {/*<Footer />*/}
 
       </div>
