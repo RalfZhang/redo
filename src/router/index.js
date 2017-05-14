a68bd7e99798b110b53f2433853a16a00808222a
unknown <zhangkang103@gmail.com>
2017-05-14 23:47:42 +0800

    路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,13 @@
-import { Router, Route, Link } from 'react-router'
+import React from 'react';
+import App from '../App';
+
+import { Router, Route, Link, browserHistory } from 'react-router-dom';
+import Movie from '../containers/movie/Movie'
 
 export default (
-  <Router>
-    <Route path="/" component={}>
-
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="moive" component={Movie}/>
     </Route>
   </Router>
 )
