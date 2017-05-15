b160a6f605c0ce6c1b9e07ccb290a1f922507da7
unknown <zhangkang103@gmail.com>
2017-05-16 00:27:11 +0800

    更新路由入口

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
-
-import Movie from './containers/movie/Movie'
-
-// import Routers from './router/index'
-
+import Routers from './router/index'
 
 ReactDOM.render((
-  <Router >
-    
-  <div>
-    <Route exactly pattern="/" component={App} />
-  </div>
-    {/*<Route path="/" component={App}>
-      <Route path="moive" component={Movie}/>
-    </Route>*/}
-  </Router >
+  <Routers >
+  </Routers >
+
   ), document.getElementById('root'));
