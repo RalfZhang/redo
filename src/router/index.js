9eca1f0778e4707bf856206141a5791402e76346
unknown <zhangkang103@gmail.com>
2017-05-16 00:26:54 +0800

    路由配置更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+
 import App from '../App';
-
-import { Router, Route, Link, browserHistory } from 'react-router-dom';
 import Movie from '../containers/movie/Movie'
+import Book from '../containers/book/Book'
 
-export default (
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <Route path="moive" component={Movie}/>
-    </Route>
-  </Router>
+
+function RouteNest(props){ return (
+  <Route exact={props.exact} path={props.path} render={ p => <props.component {...p} children={props.children}/> } />
+)}
+
+const Routers = props => (
+  <Router >
+    <RouteNest  path={'/app'} component={App}>
+      <RouteNest  path={'/app/book'} component={Book}/>
+      <RouteNest  path={'/app/movie'} component={Movie}/>
+    </RouteNest>
+  </Router >
 )
+
+export default Routers
