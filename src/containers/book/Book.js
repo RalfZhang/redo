e8281f6a154f780f8e1fe8e523be11520806359c
unknown <zhangkang103@gmail.com>
2017-05-15 23:47:29 +0800

    add book

---
+++ src/containers/book/Book.js
@@ -0,0 +1,27 @@
+/**
+ * Book
+ * @authors RalfZ (ralfz.zhang@gmail.com)
+ * @date    2017-05-15 00:00:41
+ * @version 1.0.0
+ */
+
+/**
+ * Body
+ * @authors RalfZ (ralfz.zhang@gmail.com)
+ * @date    2017-05-09 23:35:11
+ * @version 1.0.0
+ */
+
+
+import React, { Component } from 'react';
+
+
+export default class Movie extends Component {
+  render() {
+    return (
+      <div>
+        boook
+      </div>
+    )
+  }
+}
