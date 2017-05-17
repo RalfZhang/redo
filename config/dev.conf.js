ad35d2659d7ad387f5f7a96742f45cb0678491da
unknown <zhangkang103@gmail.com>
2017-05-17 23:57:20 +0800

    更新图片转发

--- config/dev.conf.js
+++ config/dev.conf.js
@@ -6,6 +6,18 @@ module.exports = {
       pathRewrite: {
         '^/api': '',
       },
+    },
+    '/img': {
+      target: 'https://img3.doubanio.com',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/img': '',
+      },
+      onProxyReq(proxyReq, req, res) {
+        // add custom header to request
+        proxyReq.setHeader('user-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
+        // or log the req
+      }
     }
   }
 }
