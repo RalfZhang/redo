95ae02c2fa50e7f3c9c6862a1c65b580c40606d2
unknown <zhangkang103@gmail.com>
2017-05-17 23:58:53 +0800

    预览更新

---
+++ src/containers/moment/Type3.js
@@ -0,0 +1,19 @@
+/**
+ * display style = 10003
+ * @authors RalfZ (ralfz.zhang@gmail.com)
+ * @date    2017-05-16 23:55:44
+ * @version 1.0.0
+ */
+
+
+import React, { Component } from 'react';
+
+export default class Type3 extends Component {
+  render() {
+    return (
+      <div>
+        style10003
+      </div>
+    )
+  }
+}
