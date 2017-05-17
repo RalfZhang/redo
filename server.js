834530e0eebd778738754c719d79e843aefef90d
unknown <zhangkang103@gmail.com>
2017-05-18 00:44:22 +0800

    更新开发服务

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, function(err) {
+app.listen(3002, function(err) {
   if (err) {
     return console.error(err);
   }
