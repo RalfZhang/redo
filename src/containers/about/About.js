cf557f8e3cacad3c37151bf7a427be04ce61035d
unknown <zhangkang103@gmail.com>
2017-05-18 00:42:10 +0800

    更新关于页面

---
+++ src/containers/about/About.js
@@ -0,0 +1,27 @@
+/**
+ * Book
+ * @authors RalfZ (ralfz.zhang@gmail.com)
+ * @date    2017-05-15 00:00:41
+ * @version 1.0.0
+ */
+
+import React, { Component } from 'react';
+
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <p>Author: RalfZ</p>
+        </div>
+        <div>
+          <p>E-mail: ralfz.zhang@gmail.com</p>
+        </div>
+        <div>
+          <p>GitHub: https://github.com/RalfZhang/redo</p>
+        </div>
+      </div>
+    )
+  }
+}
